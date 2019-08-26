d7f3c5054b11881e57efe1ab24c5b00c9b871cff
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-26 00:26:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
   end
 
   def songs
-    songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def add_song(song)
@@ -21,7 +21,7 @@ class Artist
   end
 
   def self.song_count
-
+    Song.
   end
 end
 
